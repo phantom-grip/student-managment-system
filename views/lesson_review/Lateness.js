355ee23a24b1d1dae4dbaeba86e28fb15fab6883
Yanis Urbis <yanis.urbis@gmail.com>
2019-03-14 09:28:32 +0300

    Switch class component to functional component, remove dead logic

--- views/lesson_review/Lateness.js
+++ views/lesson_review/Lateness.js
@@ -14,129 +14,102 @@ const Section = styled(Box).attrs({
 
 const ButtonGroup = Button.Group;
 
-export class Lateness extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      formLayout: "horizontal"
-    };
-  }
-
-  handleFormLayoutChange = e => {
-    this.setState({ formLayout: e.target.value });
-  };
-
-  render() {
-    const { formLayout } = this.state;
-    const formItemLayout =
-      formLayout === "horizontal"
-        ? {
-            labelCol: { span: 4 },
-            wrapperCol: { span: 14 }
-          }
-        : null;
-    const buttonItemLayout =
-      formLayout === "horizontal"
-        ? {
-            wrapperCol: { span: 14, offset: 4 }
-          }
-        : null;
-    return (
-      <div>
-        <Form layout={formLayout}>
-          <Section>
-            <Form.Item label="Дата занятия" {...formItemLayout}>
-              <DatePicker defaultValue={moment()} />
-            </Form.Item>
-            <Form.Item label="Опоздание" {...formItemLayout}>
-              <Radio.Group
-                defaultValue="horizontal"
-                onChange={this.handleFormLayoutChange}
-              >
-                <Radio.Button value="Без Опозданий">Без Опозданий</Radio.Button>
-                <Radio.Button value="Опоздал">Опоздал</Radio.Button>
-                <Radio.Button value="Сильно опоздал">
-                  Сильно опоздал
-                </Radio.Button>
-              </Radio.Group>
-            </Form.Item>
-          </Section>
-
-          <Section>
-            <Form.Item label="Тема домашней" {...formItemLayout}>
-              <ThemePicker />
-            </Form.Item>
-            <Form.Item label="Оценка" {...formItemLayout}>
-              <Radio.Group
-                defaultValue="horizontal"
-                onChange={this.handleFormLayoutChange}
-              >
-                <Radio.Button value="Без Опозданий">Не сделано</Radio.Button>
-                <Radio.Button value="Опоздал">Плохо</Radio.Button>
-                <Radio.Button value="Сильно опоздал">Хорошо</Radio.Button>
-                <Radio.Button value="Сильно опоздал">Отлично</Radio.Button>
-              </Radio.Group>
-            </Form.Item>
-          </Section>
-
-          <Section>
-            <Form.Item label="Тема теста" {...formItemLayout}>
-              <ThemePicker />
-            </Form.Item>
-            <Form.Item label="Оценка" {...formItemLayout}>
-              <Radio.Group
-                defaultValue="horizontal"
-                onChange={this.handleFormLayoutChange}
-              >
-                <Radio.Button value="Без Опозданий">10+</Radio.Button>
-                <Radio.Button value="Опоздал">20+</Radio.Button>
-                <Radio.Button value="Сильно опоздал">30+</Radio.Button>
-                <Radio.Button value="Сильно опоздал">40+</Radio.Button>
-                <Radio.Button value="Сильно опоздал">50+</Radio.Button>
-                <Radio.Button value="Без Опозданий">60+</Radio.Button>
-                <Radio.Button value="Опоздал">70+</Radio.Button>
-                <Radio.Button value="Сильно опоздал">80+</Radio.Button>
-                <Radio.Button value="Сильно опоздал">90+</Radio.Button>
-              </Radio.Group>
-            </Form.Item>
-          </Section>
-
-          <Section>
-            <Form.Item label="Тема занятия" {...formItemLayout}>
-              <ThemePicker />
-            </Form.Item>
-            <Form.Item label="Оценка" {...formItemLayout}>
-              <Radio.Group
-                defaultValue="horizontal"
-                onChange={this.handleFormLayoutChange}
-              >
-                <Radio.Button value="Без Опозданий">Плохо</Radio.Button>
-                <Radio.Button value="Опоздал">Хорошо</Radio.Button>
-                <Radio.Button value="Сильно опоздал">Отлично</Radio.Button>
-              </Radio.Group>
-            </Form.Item>
-          </Section>
-
-          <Section>
-            <Form.Item label="Комментарий" {...formItemLayout}>
-              <LessonComment />
-            </Form.Item>
-          </Section>
-
-          <Section>
-            <Form.Item label="Аванс" {...formItemLayout}>
-              <LessonCreditSelector />
-            </Form.Item>
-            <Form.Item label="Долг" {...formItemLayout}>
-              <LessonCreditSelector />
-            </Form.Item>
-          </Section>
-
-          <Form.Item {...buttonItemLayout}>
-            <Button type="primary">Submit</Button>
+export const Lateness = () => {
+  const formLayout = "horizontal";
+  const formItemLayout =
+    formLayout === "horizontal"
+      ? {
+          labelCol: { span: 4 },
+          wrapperCol: { span: 14 }
+        }
+      : null;
+  const buttonItemLayout =
+    formLayout === "horizontal"
+      ? {
+          wrapperCol: { span: 14, offset: 4 }
+        }
+      : null;
+  return (
+    <div>
+      <Form layout={formLayout}>
+        <Section>
+          <Form.Item label="Дата занятия" {...formItemLayout}>
+            <DatePicker defaultValue={moment()} />
           </Form.Item>
-        </Form>
-      </div>
-    );
-  }
-}
+          <Form.Item label="Опоздание" {...formItemLayout}>
+            <Radio.Group defaultValue="horizontal">
+              <Radio.Button value="Без Опозданий">Без Опозданий</Radio.Button>
+              <Radio.Button value="Опоздал">Опоздал</Radio.Button>
+              <Radio.Button value="Сильно опоздал">Сильно опоздал</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+        </Section>
+
+        <Section>
+          <Form.Item label="Тема домашней" {...formItemLayout}>
+            <ThemePicker />
+          </Form.Item>
+          <Form.Item label="Оценка" {...formItemLayout}>
+            <Radio.Group defaultValue="horizontal">
+              <Radio.Button value="Без Опозданий">Не сделано</Radio.Button>
+              <Radio.Button value="Опоздал">Плохо</Radio.Button>
+              <Radio.Button value="Сильно опоздал">Хорошо</Radio.Button>
+              <Radio.Button value="Сильно опоздал">Отлично</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+        </Section>
+
+        <Section>
+          <Form.Item label="Тема теста" {...formItemLayout}>
+            <ThemePicker />
+          </Form.Item>
+          <Form.Item label="Оценка" {...formItemLayout}>
+            <Radio.Group defaultValue="horizontal">
+              <Radio.Button value="Без Опозданий">10+</Radio.Button>
+              <Radio.Button value="Опоздал">20+</Radio.Button>
+              <Radio.Button value="Сильно опоздал">30+</Radio.Button>
+              <Radio.Button value="Сильно опоздал">40+</Radio.Button>
+              <Radio.Button value="Сильно опоздал">50+</Radio.Button>
+              <Radio.Button value="Без Опозданий">60+</Radio.Button>
+              <Radio.Button value="Опоздал">70+</Radio.Button>
+              <Radio.Button value="Сильно опоздал">80+</Radio.Button>
+              <Radio.Button value="Сильно опоздал">90+</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+        </Section>
+
+        <Section>
+          <Form.Item label="Тема занятия" {...formItemLayout}>
+            <ThemePicker />
+          </Form.Item>
+          <Form.Item label="Оценка" {...formItemLayout}>
+            <Radio.Group defaultValue="horizontal">
+              <Radio.Button value="Без Опозданий">Плохо</Radio.Button>
+              <Radio.Button value="Опоздал">Хорошо</Radio.Button>
+              <Radio.Button value="Сильно опоздал">Отлично</Radio.Button>
+            </Radio.Group>
+          </Form.Item>
+        </Section>
+
+        <Section>
+          <Form.Item label="Комментарий" {...formItemLayout}>
+            <LessonComment />
+          </Form.Item>
+        </Section>
+
+        <Section>
+          <Form.Item label="Аванс" {...formItemLayout}>
+            <LessonCreditSelector />
+          </Form.Item>
+          <Form.Item label="Долг" {...formItemLayout}>
+            <LessonCreditSelector />
+          </Form.Item>
+        </Section>
+
+        <Form.Item {...buttonItemLayout}>
+          <Button type="primary">Submit</Button>
+        </Form.Item>
+      </Form>
+    </div>
+  );
+};
